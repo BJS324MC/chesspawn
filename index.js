003345413ba915118093df2d620b5f6c303eb5b6
??? <bjs332244@gmail.com>
2021-10-22 13:39:08 +0800

    again

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-app.get('/', (request, response) => response.send('<h1>Chess Pawn!</h1>'));
 app.get('/editor', (request, response) => response.send('<h1>Time to edit!</h1>'));
 app.get('/explorer', (request, response) => response.send('<h1>Time to explore!</h1>'));
 app.use(express.static("public",{
